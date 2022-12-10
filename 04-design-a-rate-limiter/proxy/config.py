956fda565e51656ad115103b54487b11d5e43fa5
Dowon Oh <dhehdnjs03@gmail.com>
2022-12-11 00:47:16 +0900

    04: proxy: config: add refill_period env

--- 04-design-a-rate-limiter/proxy/config.py
+++ 04-design-a-rate-limiter/proxy/config.py
@@ -5,7 +5,8 @@ class Settings(BaseSettings):
     api_server_host: str = "0.0.0.0"
     api_server_port: int = 9999
     capacity: int = 1000
-    number_of_refilled_tokens_per_second: int = 10
+    refill_period: int = 1
+    number_of_refilled_tokens_per_period: int = 10
 
 
 settings = Settings()
