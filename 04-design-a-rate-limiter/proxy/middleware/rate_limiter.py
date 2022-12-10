1c2f9e383e6d0dc32e2b0a288346a2bca00977e0
Dowon Oh <dhehdnjs03@gmail.com>
2022-12-11 01:36:37 +0900

    04: proxy: middleware: rate_limiter: rate limit middleware

---
+++ 04-design-a-rate-limiter/proxy/middleware/rate_limiter.py
@@ -0,0 +1,27 @@
+from starlette import status
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+
+from proxy.rate_limiter import endpoint_rate_limiter, TokenBucket, RequestType
+
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        request_type = RequestType(path=request.url.path, method=request.method)
+
+        if request.url.path in endpoint_rate_limiter:
+            rate_limiter = endpoint_rate_limiter[request_type]
+        else:
+            rate_limiter = TokenBucket()
+            endpoint_rate_limiter[request_type] = rate_limiter
+
+        if rate_limiter.consume():
+            response = await call_next(request)
+        else:
+            response = Response(status_code=status.HTTP_429_TOO_MANY_REQUESTS)
+
+        response.headers["X-Ratelimit-Remaining"] = str(rate_limiter.get_remaining_tokens())
+        response.headers["X-Ratelimit-Limit"] = str(rate_limiter.get_capacity())
+        response.headers["X-Ratelimit-Retry-After"] = str(rate_limiter.get_refill_period())
+        return response
