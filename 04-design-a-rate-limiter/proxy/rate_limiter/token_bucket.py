7a7d308a9ceb28cb89f1b1ad5545a193765a09bc
Dowon Oh <dhehdnjs03@gmail.com>
2022-12-11 01:32:16 +0900

    04: proxy: token_bucket: fix refill algorithm

--- 04-design-a-rate-limiter/proxy/rate_limiter/token_bucket.py
+++ 04-design-a-rate-limiter/proxy/rate_limiter/token_bucket.py
@@ -5,15 +5,14 @@ from proxy.config import settings
 
 class TokenBucket:
     def __init__(self):
-        self._capacity = settings.capacity
-        self._refill_period = settings.refill_period
-        self._number_of_refilled_tokens_per_period = settings.number_of_refilled_tokens_per_period
-        self._number_of_refilled_tokens_per_second = self._number_of_refilled_tokens_per_period // self._refill_period
-        self._tokens = self._capacity
+        self._capacity: int = settings.capacity
+        self._refill_period: int = settings.refill_period
+        self._number_of_refilled_tokens_per_period: int = settings.number_of_refilled_tokens_per_period
+        self._tokens: int = self._capacity
         self._last_refill_time = int(time.time())
 
-    def _is_empty(self):
-        return self._tokens == 0
+    def _is_empty(self) -> bool:
+        return self._tokens < 1
 
     def consume(self, tokens: int = 1) -> bool:
         self._refill()
@@ -22,7 +21,7 @@ class TokenBucket:
             return True
         return False
 
-    def get_remaining_tokens(self) -> int:
+    def get_remaining_tokens(self) -> float:
         return self._tokens
 
     def get_capacity(self) -> int:
@@ -31,8 +30,13 @@ class TokenBucket:
     def get_refill_period(self) -> int:
         return self._refill_period
 
+    def _get_refilled_tokens(self, elapsed: int) -> int:
+        refill_count = elapsed // self._refill_period
+        return refill_count * self._number_of_refilled_tokens_per_period
+
     def _refill(self) -> None:
         now = int(time.time())
         elapsed = now - self._last_refill_time
-        self._last_refill_time = now
-        self._tokens = min(self._capacity, self._tokens + elapsed * self._number_of_refilled_tokens_per_second)
+        if elapsed >= self._refill_period:
+            self._last_refill_time = now
+            self._tokens = min(self._capacity, self._tokens + self._get_refilled_tokens(elapsed))
