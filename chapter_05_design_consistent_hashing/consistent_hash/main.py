2386d5e16206cbe86e5d082cfe55e1bcaf38ff40
Dowon Oh <dhehdnjs03@gmail.com>
2022-12-26 02:00:23 +0900

    05: consistent_hash: add startup event

--- chapter_05_design_consistent_hashing/consistent_hash/main.py
+++ chapter_05_design_consistent_hashing/consistent_hash/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from starlette.requests import Request
 
+from consistent_hash.consistent_hash import ConsistentHash, CacheServer
 from consistent_hash.dto import ReadRequest, WriteRequest
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+async def startup(request: Request):
+    request.state.consistent_hash = ConsistentHash([CacheServer()])
 
 @app.post("/read")
 async def read(request_data: ReadRequest):
