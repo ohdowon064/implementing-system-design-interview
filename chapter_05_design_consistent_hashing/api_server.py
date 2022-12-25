1eb281b2a7d9bca8eef17622d1812a5f0ff8f432
Dowon Oh <dhehdnjs03@gmail.com>
2022-12-26 03:34:37 +0900

    05: set headers

--- chapter_05_design_consistent_hashing/api_server.py
+++ chapter_05_design_consistent_hashing/api_server.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 
 from cache_manager import cache
 
@@ -17,16 +17,27 @@ async def key_error_handler(request: Request, exc: KeyError):
     )
 
 
-@app.get("/{key}")
-async def read(key: str):
-    return cache.read(key)
+@app.get("/{key}", status_code=200)
+async def read(response: Response, key: str):
+    result = cache.read(key)
+    response.headers["X-CacheServer-Index"] = str(result.cache_server_index)
+    response.headers["X-CacheServer-Count"] = str(result.cache_server_count)
+    return result.dict(include={"value"})
 
 
 class WriteRequest(BaseModel):
     value: str
 
 
-@app.post("/{key}")
-async def write(key: str, request_body: WriteRequest):
-    cache.set(key, request_body.value)
+@app.post("/{key}", status_code=201)
+async def write(response: Response, key: str, request_body: WriteRequest):
+    result = cache.set(key, request_body.value)
+    response.headers["X-CacheServer-Index"] = str(result.cache_server_index)
+    response.headers["X-CacheServer-Count"] = str(result.cache_server_count)
     return {"message": "Value set."}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("api_server:app", host="0.0.0.0", port=8000, reload=True)
--- chapter_05_design_consistent_hashing/cache_manager.py
+++ chapter_05_design_consistent_hashing/cache_manager.py
@@ -1,18 +1,38 @@
 import requests
+from pydantic import BaseModel
+
+
+class RequestResult(BaseModel):
+    cache_server_index: int
+    cache_server_count: int
+
+
+class ReadResult(RequestResult):
+    value: str
 
 
 class CacheManager:
     def __init__(self):
         self._SERVER_ADDR = "http://localhost:9999"
 
-    def read(self, key: str) -> str:
-        res = requests.get(f"self._SERVER_ADDR/{key}")
+    def read(self, key: str) -> ReadResult:
+        res = requests.get(f"{self._SERVER_ADDR}/{key}", timeout=3)
         if res.status_code == 404:
             raise KeyError("Key does not exist")
-        return res.json()["value"]
+        return ReadResult(
+            cache_server_index=res.headers["X-CacheServer-Index"],
+            cache_server_count=res.headers["X-CacheServer-Count"],
+            value=res.json()["value"],
+        )
 
-    def set(self, key: str, value: str) -> None:
-        requests.post(f"self._SERVER_ADDR/{key}", json={"value": value})
+    def set(self, key: str, value: str) -> RequestResult:
+        res = requests.post(
+            f"{self._SERVER_ADDR}/{key}", json={"value": value}, timeout=3
+        )
+        return RequestResult(
+            cache_server_index=res.headers["X-CacheServer-Index"],
+            cache_server_count=res.headers["X-CacheServer-Count"],
+        )
 
 
 cache = CacheManager()
