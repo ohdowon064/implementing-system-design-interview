81d859c94599be70198cfad9feaa909f2e4a0180
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-15 20:25:47 +0900

    put, get

--- chapter_06_design_a_key_value_store/src/coordinator/coordinator.service.ts
+++ chapter_06_design_a_key_value_store/src/coordinator/coordinator.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConsistentHash, Node } from './consistentHash';
 @Injectable()
-export class CoordinatorService {}
+export class CoordinatorService {
+  nodes: Node[] = [new Node(1), new Node(2), new Node(3)];
+  consistentHash: ConsistentHash = new ConsistentHash(this.nodes);
+
+  put(key: string, value: string): void {
+    const node = this.consistentHash.findNodeByKey(key);
+    node.put(key, value);
+  }
+
+  get(key: string): string {
+    const node = this.consistentHash.findNodeByKey(key);
+    const value = node.get(key);
+    if (!value) {
+      throw new NotFoundException(`Key ${key} not found`);
+    }
+    return value;
+  }
+}
