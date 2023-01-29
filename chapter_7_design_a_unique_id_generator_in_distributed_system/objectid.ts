ca5ffacd39a483de83aa276ff8980493d22ea6fb
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-29 20:40:37 +0900

    07: get objectid

--- chapter_7_design_a_unique_id_generator_in_distributed_system/objectid.ts
+++ chapter_7_design_a_unique_id_generator_in_distributed_system/objectid.ts
@@ -1,30 +1,54 @@
 import {randomBytes} from "crypto";
+import AsyncLock = require("async-lock");
 
 function getCounter() {
     return Math.floor(Math.random() * (0xFFFFFF + 1));
 }
 
 class ObjectID{
+    private id: string;
     private static pid = process.pid;
-    private static randPart = randomBytes(5);
-    private inc = getCounter();
+    private static rand = randomBytes(5);
+    private static inc = getCounter();
+    private static lock = new AsyncLock();
 
-    private getProcessRandom() {
+    private getProcessRandom(): Buffer {
         const pid = process.pid;
         if(pid != ObjectID.pid) {
             ObjectID.pid = pid;
-            ObjectID.randPart = randomBytes(5);
+            ObjectID.rand = randomBytes(5);
         }
-        return ObjectID.randPart;
+        return ObjectID.rand;
+    }
+
+    private static increment(): void {
+        ObjectID.inc = (ObjectID.inc + 1) % (0xFFFFFF + 1);
+    }
+
+    private getTimestampPart(): string {
+        return Math.floor(Date.now() / 1000).toString(16);
+    }
+
+    private getRandomPart(): string {
+        return this.getProcessRandom().toString("hex");
+    }
+
+    private getCounterPart(): string {
+        return ObjectID.inc.toString(16);
     }
 
     constructor() {
-        const timestamp = Math.floor(Date.now() / 1000).toString(16);
-        const random = this.getProcessRandom().toString("hex");
-        const counter = this.inc.toString(16);
-        const objectId = `${timestamp}${random}${counter}`;
+        const timestamp = this.getTimestampPart();
+        const random = this.getRandomPart();
 
+        let objectId = `${timestamp}${random}`;
 
+        ObjectID.lock.acquire("inc", (done) => {
+            objectId += this.getCounterPart();
+            ObjectID.increment();
+        });
+
+        this.id = objectId;
     }
 
 }
