4948d0f02b8a88079539f31bdc9ad5b3e95e2587
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-29 22:22:59 +0900

    07: number objectid

---
+++ chapter_7_design_a_unique_id_generator_in_distributed_system/objectid.only.number.ts
@@ -0,0 +1,57 @@
+import {randomBytes} from "crypto";
+import Mutex from "./mutex";
+
+function getCounter() {
+    return Math.floor(Math.random() * (0xFFFFFF + 1));
+}
+
+class ObjectID {
+    private readonly id: string;
+    private static pid = process.pid;
+    private static rand = randomBytes(5);
+    private static inc = getCounter();
+    private static mutex = new Mutex();
+
+    private getProcessRandom(): Buffer {
+        const pid = process.pid;
+        if (pid != ObjectID.pid) {
+            ObjectID.pid = pid;
+            ObjectID.rand = randomBytes(5);
+        }
+        return ObjectID.rand;
+    }
+
+    private static increment(): void {
+        ObjectID.inc = (ObjectID.inc + 1) % (0xFFFFFF + 1);
+    }
+
+    private getTimestampPart(): number {
+        return Math.floor(Date.now() / 1000)
+    }
+
+    private getRandomPart(): number {
+        return parseInt(this.getProcessRandom().toString("hex"), 16)
+    }
+
+    private getCounterPart(): number {
+        return ObjectID.inc;
+    }
+
+    constructor() {
+        const timestamp = this.getTimestampPart();
+        const random = this.getRandomPart();
+        const counter = this.getCounterPart();
+
+        this.id = `${timestamp}${random}${counter}`;
+        ObjectID.increment();
+    }
+
+
+    public toString(): string {
+        return this.id;
+    }
+}
+
+for(let i = 0; i < 100; i++) {
+    console.log(new ObjectID())
+}
