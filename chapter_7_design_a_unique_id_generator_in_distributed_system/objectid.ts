ab03010fccee1bac930fa897ee033eb99e5ce7ca
Dowon Oh <dhehdnjs03@gmail.com>
2023-01-29 20:27:03 +0900

    07: create objectid by timestamp, random

--- chapter_7_design_a_unique_id_generator_in_distributed_system/objectid.ts
+++ chapter_7_design_a_unique_id_generator_in_distributed_system/objectid.ts
@@ -5,11 +5,25 @@ function getCounter() {
 }
 
 class ObjectID{
-    private pid = process.pid;
-    private randPart = randomBytes(5);
+    private static pid = process.pid;
+    private static randPart = randomBytes(5);
     private inc = getCounter();
 
+    private getProcessRandom() {
+        const pid = process.pid;
+        if(pid != ObjectID.pid) {
+            ObjectID.pid = pid;
+            ObjectID.randPart = randomBytes(5);
+        }
+        return ObjectID.randPart;
+    }
+
     constructor() {
+        const timestamp = Math.floor(Date.now() / 1000).toString(16);
+        const random = this.getProcessRandom().toString("hex");
+        const counter = this.inc.toString(16);
+        const objectId = `${timestamp}${random}${counter}`;
+
 
     }
 
